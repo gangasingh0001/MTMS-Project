ab07716cacc0561eb165e9b91951d816bd59376b
GangaSingh <46618088+gangasingh0001@users.noreply.github.com>
2023-04-05 11:42:19 -0400

    Added Server

---
+++ src/main/java/Replicas/Server/Constant/Common.java
@@ -0,0 +1,4 @@
+package Replicas.Server.Constant;
+
+public class Common {
+}
---
+++ src/main/java/Replicas/Server/Constant/ServerConstant.java
@@ -0,0 +1,16 @@
+package Replicas.Server.Constant;
+
+public class ServerConstant {
+    public static final int SERVER_ATWATER_PORT = 2510;
+    public static final int SERVER_VERDUN_PORT = 2511;
+    public static final int SERVER_OUTREMONT_PORT = 2512;
+    public static final String SERVER_ATWATER_PREFIX = "ATW";
+    public static final String SERVER_VERDUN_PREFIX = "VER";
+    public static final String SERVER_OUTREMONT_PREFIX = "OUT";
+    public static final String SERVER_ATWATER = "ATWATER";
+    public static final String SERVER_VERDUN = "VERDUN";
+    public static final String SERVER_OUTREMONT = "OUTREMONT";
+    public static final String ADD_MOVIE_SLOTS = "Add Movie Slots";
+    public static final String REMOVE_MOVIE_SLOTS = "Remove Movie Slots";
+    public static final String LIST_MOVIE_SHOWS_AVAILABILITY = "List Movie Shows Availability";
+}
